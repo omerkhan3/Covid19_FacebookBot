fff31853c553912af9a94205f245324470e6b864
AnthonyFante <aafante@syr.edu>
2020-03-29 16:15:47 -0400

    Added scheduling job & some logging

--- bot.py
+++ bot.py
@@ -3,10 +3,12 @@ import json
 import requests
 from flask import Flask, request
 from pprint import pprint
-#https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
-#https://www.twilio.com/blog/2017/12/facebook-messenger-bot-python.html
-#logger = logging.getLogger()
-#logger.setLevel(logging.INFO)
+from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
+import atexit
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -22,15 +24,18 @@ class StateData:
 
     def constructStateData(self, state):
         url = self.url + state.upper()
+        logger.info(url)
         resp = requests.get(url)
         clean = resp.text.split('\n')[-1]
         cleaner = clean.split(',')[2]
+        logger.info(cleaner)
         return cleaner
 
     def buildStates(self):
         """Simple function to iterate list argument to pull data"""
         stateData = []
         for state in self.stateList:
+            logger.info(state)
             data = self.constructStateData(state)
             self.stateData[state] = data
 
@@ -43,6 +48,9 @@ class CountryData:
     def getCountriesTotal(self):
         return self,countriesTotal
 
+    def __call__(self):
+        return self.countriesTotals
+
     def getCountriesMapping(self):
         return self.countriesMapping
 
@@ -53,7 +61,9 @@ class CountryData:
         payload = {}
         headers = {}
         curl = self.url + "summary"
+        logger.info(curl)
         countries = requests.request("GET", curl, headers=headers, data = payload)
+        logger.info(countries.status_code)
         return countries.json()
 
     def getCountryTotals(self):
@@ -61,6 +71,7 @@ class CountryData:
         headers = {}
         countries = self.getCountriesSummary()
         for each in countries["Countries"]:
+            logger.info(each)
             self.countriesTotals[each['Country']] = each
         return self.countriesTotals
 
@@ -70,13 +81,14 @@ stateData = StateData()
 @app.route("/countryTotals", methods=['GET', 'POST'])
 def getCountryData():
     global countryData
+    logger.info(str(countryData.getCountryTotals()))
     return str(countryData.getCountryTotals())
 
 @app.route("/stateTotals", methods=['GET', 'POST'])
 def getStateData():
     global stateData
+    logger.info(str(stateData.getStateTotals()))
     return str(stateData.getStateTotals())
-
 
 # def main():
 #     global countryData
@@ -88,6 +100,11 @@ def getStateData():
 #
 # main()
 
-
 if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=getCountryData, trigger='interval', seconds=5)
+    scheduler.add_job(func=getStateData, trigger='interval', seconds=6)
+    scheduler.start()
     app.run(debug=True)
+
+    atexit.register(lambda: scheduler.shutdowm())
